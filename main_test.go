85b455742dea8e928b70c5403ed055c77f4f26eb
Jonathan Teixeira <jonathancateixeira@outlook.com>
2024-01-05 16:53:15 -0300

    executing test

--- main_test.go
+++ main_test.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"strconv"
 	"testing"
 
 	"github.com/gin-gonic/gin"
@@ -17,14 +19,14 @@ import (
 var ID int
 
 func SetupRoutesTests() *gin.Engine {
-
+	gin.SetMode(gin.ReleaseMode)
 	routes := gin.Default()
 
 	return routes
 }
 
 func CreatAlunoMock() {
-	aluno := models.Aluno{Nome: "Aluno Teste", CPF: "12345678910", RG: "123456789"}
+	aluno := models.Aluno{Nome: "Aluno Teste", CPF: "12345678911", RG: "123456789"}
 	database.DB.Create(&aluno)
 
 	ID = int(aluno.ID)
@@ -71,4 +73,72 @@ func TestListingAllAlunosHandler(t *testing.T) {
 	assert.Equal(t, http.StatusOK, response.Code)
 
 	fmt.Println(response.Body)
+}
+
+func TestGetAlunoCPFHandler(t *testing.T) {
+	database.ConnectionDB()
+
+	CreatAlunoMock()
+	defer DeleteAlunoMock()
+
+	r := SetupRoutesTests()
+
+	r.GET("/alunos/cpf/:cpf", controllers.SearchAlunoCPF)
+
+	req, _ := http.NewRequest("GET", "/alunos/cpf/12345678911", nil)
+
+	response := httptest.NewRecorder()
+
+	r.ServeHTTP(response, req)
+
+	assert.Equal(t, http.StatusOK, response.Code)
+}
+
+func TestGetAlunoID(t *testing.T) {
+	database.ConnectionDB()
+
+	CreatAlunoMock()
+	defer DeleteAlunoMock()
+
+	r := SetupRoutesTests()
+
+	r.GET("/alunos/:id", controllers.GetAlunoId)
+
+	path := "/alunos/" + strconv.Itoa(ID)
+
+	req, _ := http.NewRequest("GET", path, nil)
+
+	response := httptest.NewRecorder()
+
+	r.ServeHTTP(response, req)
+
+	var alunoMock models.Aluno
+
+	json.Unmarshal(response.Body.Bytes(), &alunoMock)
+
+	fmt.Println(alunoMock.Nome)
+
+	assert.Equal(t, "Aluno Teste", alunoMock.Nome)
+	assert.Equal(t, "12345678911", alunoMock.CPF)
+	assert.Equal(t, "123456789", alunoMock.RG)
+	assert.Equal(t, http.StatusOK, response.Code)
+}
+
+func TestDeleteAluno(t *testing.T) {
+	database.ConnectionDB()
+
+	CreatAlunoMock()
+
+	r := SetupRoutesTests()
+	r.DELETE("/alunos/:id", controllers.DeleteAluno)
+
+	path := "/alunos/" + strconv.Itoa(ID)
+
+	req, _ := http.NewRequest("DELETE", path, nil)
+
+	response := httptest.NewRecorder()
+
+	r.ServeHTTP(response, req)
+
+	assert.Equal(t, http.StatusOK, response.Code)
 }
